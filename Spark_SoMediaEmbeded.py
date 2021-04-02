360a245784acd5dd625fefc0461d9e8ef0561767
Grisworld <oguzandas@hotmail.com>
2021-04-02 22:48:15 +0300

    Some basic input code has been implemented.

--- Spark_SoMediaEmbeded.py
+++ Spark_SoMediaEmbeded.py
@@ -1,2 +1,23 @@
+import pandas as pd
+
 if __name__ == '__main__':
-    print("ad")
+    while True:
+        permission = input("Would you like to inspect a CSV File or download a file(CSV/DATA)?")
+        if permission == "CSV":
+            csv_file = input("Please enter your csv file")
+            csv_data = pd.read_csv("D:\\PyCharmProjects\\BIL401Project\\csv_files\\"+csv_file)
+
+            break
+        elif permission == "DATA":
+            while True:
+                yot = input("Would you like to gather YouTube or Twitter a file(YOUTUBE/TWITTER)?")
+                if permission == "YOUTUBE":
+
+                    break
+                elif permission == "TWITTER":
+                    break
+                else:
+                    print("Please enter YOUTUBE or TWITTER")
+            break
+        else:
+            print("Please enter CSV or DATA")
