d216f8aa222e737c01fcb53f7d58e72c85f2b131
Grisworld <oguzandas@hotmail.com>
2021-04-04 16:28:09 +0300

    Spark and Twitter_Youtube are Merged(2)

--- Spark_SoMediaEmbeded.py
+++ Spark_SoMediaEmbeded.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import pyspark as ps
+from pyspark.ml import PipelineModel
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, monotonically_increasing_id, udf, avg
 from SparkMl_NGrams import cleanData
@@ -13,26 +14,29 @@ findspark.init()
 
 
 def readfile(filename):
-    file_df = spark.read \
-        .format("csv") \
-        .option("header", "false") \
-        .load(filename)
+    try:
+        file_df = spark.read \
+            .format("csv") \
+            .option("header", "false") \
+            .load(filename)
 
-    file_df = file_df \
-        .withColumn("index", monotonically_increasing_id()) \
-        .withColumnRenamed("_c0", "target") \
-        .withColumnRenamed("_c5", "text")
-    file_df = file_df.select("index", "text", "target")
+        file_df = file_df \
+            .withColumn("index", monotonically_increasing_id()) \
+            .withColumnRenamed("_c0", "target") \
+            .withColumnRenamed("_c5", "text")
+        file_df = file_df.select("index", "text", "target")
 
-    file_df.show(5)
+        file_df.show(5)
 
-    convert_udf = udf(lambda z: cleanData(z))
-    file_df = file_df.select("index", convert_udf(col("text")).alias("text"), "target")
+        convert_udf = udf(lambda z: cleanData(z))
+        file_df = file_df.select("index", convert_udf(col("text")).alias("text"), "target")
 
-    file_df.show(5)
+        file_df.show(5)
 
-    print("Number of entries: ", file_df.count())
-    return file_df
+        print("Number of entries: ", file_df.count())
+        return file_df
+    except:
+        print("File cannot found!!")
 
 
 if __name__ == '__main__':
@@ -42,18 +46,24 @@ if __name__ == '__main__':
     while True:
         permission = input("Would you like to inspect a CSV File or download a file(CSV/DATA)? ")
         if permission == "CSV":
-            csv_file = input("Please enter your csv file")
             conf = ps.SparkConf() \
                 .setMaster("local[*]") \
                 .setAppName("Merging")
             spark = SparkSession.builder \
                 .config(conf=conf) \
                 .getOrCreate()
+            csv_file = input("Please enter your csv file=  ") #training.1600000.processed.noemoticon.csv
+
             csv_data = readfile("D:\\PyCharmProjects\\BIL401Project\\csv_files\\" + csv_file)
             if csv_data is None:
                 print("Failed to File Open...")
             else:
-                csv_data.select(avg("prediction")).show()
+                (train_set, test_set) = csv_data.randomSplit([0.9, 0.1], seed=2000)
+
+                model = PipelineModel.load('finalized_model')
+
+                predictions = model.transform(test_set)
+                predictions.select(avg("prediction")).show()
             break
         elif permission == "DATA":
             while True:
